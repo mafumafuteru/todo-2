9020a7be1795447b1b91dbd5fd21750ee7e9872a
Jiawen <jiawen.huang@code.berlin>
2022-11-18 15:37:33 +0100

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, flash, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
@@ -7,6 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+import time
 
 
 app = Flask(__name__)
@@ -24,9 +25,7 @@ app.app_context().push()
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200),nullable=False)
-    rating = db.Column(db.Integer,nullable=False)
-    completed = db.Column(db.Integer, default=0)
+    content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -44,19 +43,19 @@ class User(db.Model, UserMixin):
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Username"})
 
-    password = PasswordField(validators=[InputRequired(),Length(min=4,max=20)], render_kw={"placeholder": "Password"})
+    password = PasswordField(validators=[InputRequired(),Length(min=8,max=20)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField("Register")
 
     def validate_username(self, username):
         existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
-            raise ValidationError("That username already exists. Please choose a different one.")
+            raise ValidationError('That username already exists. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Username"})
 
-    password = PasswordField(validators=[InputRequired(),Length(min=4,max=20)], render_kw={"placeholder": "Password"})
+    password = PasswordField(validators=[InputRequired(),Length(min=8,max=20)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField("Login")
 
@@ -69,8 +68,9 @@ def home():
 def addTask():
     if request.method=='POST':
         task_content = request.form['content']
+        if(task_content == ""):
+            return 'Please fill out the text field'
         new_task = Todo(content=task_content)
-
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -91,25 +91,33 @@ def login():
             if bcrypt.check_password_hash(user.password, form.password.data):
                login_user(user)
                return  redirect(url_for('addTask'))
+            else:
+                flash("Password entered is wrong")
+        else:
+            flash("Account not found")
     return render_template('login.html', form=form)
 
 @app.route('/logout', methods=['GET','POST'])
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('home'))
 
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = RegisterForm()
-
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-
+    if request.method == "POST":
+        if form.validate_on_submit():
+            try:
+                hashed_password = bcrypt.generate_password_hash(form.password.data)
+                new_user = User(username=form.username.data, password=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+            except:
+                flash("Something went wrong", "error")
+            return redirect(url_for('login'))
+        else:
+            flash("Sorry, this username already exists, please try a new one.", "error")
     return render_template('register.html', form=form)
 
 @app.route('/delete/<int:id>')
